0710da8ecc51b96213d5161ca90dbe222d00a129
Salm-a-n <salmanntechs@gmail.com>
2025-10-28 21:02:36 +0530

    Add home view to main URL configuration and remove it from student URLs

--- clg_stm/students/urls.py
+++ clg_stm/students/urls.py
@@ -2,6 +2,5 @@ from django.urls import path,include
 from students import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
     path('<str:msg>/', views.student_list, name='list'),
 ]
